fa09793ffc492119ce327c6a1c3cd6d532ced59e
INDRANIL MAITI <112776618+Indra-photon@users.noreply.github.com>
2024-08-06 10:42:29 +0000

    string review complete

--- 01_basics/05_strings.js
+++ 01_basics/05_strings.js
@@ -26,3 +26,32 @@ const url = 'https://hitesh.com/hitesh%20choudhury'
 console.log(url.replace('%20', '-'))
 
 console.log(url.includes('hitesh'))
+
+// String.prototype.at() : The at() method of String values takes an integer value and returns a new String
+
+const herName = new String ("Archita")
+console.log(herName.at(3))
+
+//The charCodeAt() method of String values returns an integer between 0 and 65535 representing the UTF-16 code unit at the given index.
+
+console.log(herName.charCodeAt(3))
+
+// The endsWith() method of String values determines whether a string ends with the characters of this string, returning true or false as appropriate.
+
+const str1 = new String('Cats are the best!');
+console.log(str1.endsWith('best!'));
+
+// The includes() method of String values performs a case-sensitive search to determine whether a given string may be found within this string, returning true or false as appropriate.
+
+const sentence = new String('The quick brown fox jumps over the lazy dog.');
+const word = 'fox';
+
+console.log(
+  `The word "${word}" ${
+    sentence.includes(word) ? 'is' : 'is not'
+  } in the sentence`,
+);
+
+// The indexOf() method of String values searches this string and returns the index of the first occurrence of the specified substring.
+
+// The search() method of String values executes a search for a match between a regular expression and this string, returning the index of the first match in the string.
