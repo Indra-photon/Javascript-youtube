4df97448da771ade89c2fd1088e4b5c24818a540
INDRANIL MAITI <112776618+Indra-photon@users.noreply.github.com>
2024-08-12 14:29:00 +0000

    Arrow Function

---
+++ 03_basics/03_arrowFunction.js
@@ -0,0 +1,40 @@
+const user ={
+    userName : 'Hitesh',
+    price : 999,
+    welcomeMessage : function () {
+        console.log(`${this.userName}, welcome to website`); 
+ //       console.log(this);         
+    }
+}
+user.welcomeMessage()
+user.userName = "Sam"
+user.welcomeMessage()
+
+// This -> works inside object only in JS
+
+// Basic arrow function
+const chai =(num1, num2)=>{
+    return (num1+num2)
+}
+console.log(chai(2,3));
+
+// Advanced: Implicit Return
+const addTwo =(num1, num2)=> (num1+num2)
+
+// 1. If {} is used then we have to use return statement example: chai function
+// 2. If () is used then return is not needed example: addTwo function
+
+
+
+// If we want to return an object from a function we have to wrap the () by curly braces ({}) Example below
+const addThree =(num1, num2)=> ({user: 'Hitesh'})
+console.log(addThree(3,4))
+
+
+
+
+
+
+
+
+
