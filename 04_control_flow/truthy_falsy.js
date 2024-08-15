80546648228f5030324b5018e474aa36373dfc0a
INDRANIL MAITI <112776618+Indra-photon@users.noreply.github.com>
2024-08-15 11:51:15 +0000

    ternary operator

--- 04_control_flow/truthy_falsy.js
+++ 04_control_flow/truthy_falsy.js
@@ -13,3 +13,18 @@ if (Object.keys(emptyObject).length === 0 ) {
     console.log("empty");
     
 }
+
+// nullish coalescing operator (??)
+
+let val1;
+val1 = 5 ?? 10 // It will set the value at 5 but
+val1 = null ?? 1
+0 // It will set the value 10, not null
+val1 = undefined ?? 15 // It will also set the value 15
+val1 = null ?? 10 ?? 30 // It will set the value which is set first
+
+// Ternary Operator : condition ? true : false
+const iceTeaPrice = 100
+iceTeaPrice === 100 ? console.log("True") : console.log("False");
+;
+
